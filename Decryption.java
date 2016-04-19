d00e0b4b99c46341aa9e33725581a375d80672cb
Maeyrl LYHME <callofdutyelitetest@hotmail.com>
2016-04-19 11:03:54 -0500

    Added files via upload

--- Decryption.java
+++ Decryption.java
@@ -5,124 +5,122 @@ import java.io.*;
 //Goes through Caesar Cipher and decrpyts it and prints the correct value
 
 public class Decryption {
-	public static void main(String args[]) {
-		
-		//Decrypts all Information from String.txt
-		File file = new File("string.txt");
-		ArrayList<String> words = new ArrayList<String>();
+	private File file = new File("string.txt");
+	private ArrayList<String> words = new ArrayList<String>();
+	private ArrayList<String> confirmed = new ArrayList<String>();
+	private ArrayList<String> arr = new ArrayList<String>();
+	
+	public void decodeString(String str) {
+		String cipher = "";
+		for(int in = 26; in>0; in--) {
+			int num = in;
+			for(int i = 0; i < str.length(); i++) {
+				num = in;
+				String g = "" + str.charAt(i);
+				if(g.equals("") || g.equals(" ")) {
+					cipher += " ";
+					continue;
+				}
+				char c = str.charAt(i);
+				int max = 0;
+				char defChar;
+				if(c>='a' && c<='z'){
+					max = 122;
+					defChar = 'a';
+				} else {
+					max = 90;
+					defChar = 'A';
+				}
+				
+				char testing = c;
+				for(int example=in; example>0; example--) {
+					if((int) testing == max) {
+						testing = defChar;
+					} else {
+						testing+=1;
+					}
+						
+				}
+				cipher += testing;
+				num = in;
+			}
+			arr.add(cipher);
+			cipher = "\n";
+		}
+	}
+	
+	public void pullFromDictionary() {
+		try {
+			BufferedReader brc = new BufferedReader(new FileReader("Dictionary.txt"));
+			String line = "";
+			while ((line = brc.readLine()) != null) {
+				words.add(line);
+			}
+			brc.close();
+		} catch (Exception e) {
+			System.out.println("Something went wrong...");
+		}
+	}
+	
+	public void readLinesFromEncyptedFile() {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line = "";
 			String cipher = "";
-			ArrayList<String> arr = new ArrayList<String>();
 			while ((line = br.readLine()) != null) {
-				//System.out.println("LINE: " + line);
-				for(int in = 26; in>0; in--) {
-					int num = in;
-					for(int i = 0; i < line.length(); i++) {
-						num = in;
-						String g = "" + line.charAt(i);
-						if(g.equals("") || g.equals(" ")) {
-							cipher += " ";
-							continue;
-						}
-						char c = line.charAt(i);
-						int max = 0;
-						char defChar;
-						if(c>='a' && c<='z'){
-							max = 122;
-							defChar = 'a';
-						} else {
-							max = 90;
-							defChar = 'A';
-						}
-						
-						char testing = c;
-						for(int example=in; example>0; example--) {
-							if((int) testing == max) {
-								testing = defChar;
-							} else {
-								testing+=1;
-							}
-								
-						}
-						cipher += testing;
-						num = in;
-					}
-					arr.add(cipher);
-					cipher = "\n";
-				}
+				decodeString(line);
 			}
-			
-			//Grabs all the words from the dictionary
-			BufferedReader brc = new BufferedReader(new FileReader("Dictionary.txt"));
-			while ((line = brc.readLine()) != null) {
-				words.add(line);
-			}
-			
-			//Checks to see if decrypted strings are ACTUALLY in the dictionary
-			ArrayList<String> confirmed = new ArrayList<String>();
-			for(int i = 0; i<arr.size(); i++) {
-				String g = arr.get(i);
-				//System.out.println("WORDS: " + g);
-				for(String a : words) {
-					//System.out.println("COMPARER: " + a);
-					StringTokenizer st = new StringTokenizer(g);
-					while(st.hasMoreTokens()) {
-						if(st.nextToken().equals(a)) {
-							if(!(confirmed.contains(g))) {
-								String l = g;
-								l = l.replaceAll("\n", "");
-								//System.out.println("Decoded String: " + l);
-								confirmed.add(g);
-								//System.out.println();
-							//System.out.println("STRING: " + g);
-							//System.out.println("TEST STRING: " + a);
-							} else {
-								String l = g;
-								l = l.replaceAll("\n", "");
-								//System.out.println("Preexisting String: " + l);
-								//System.out.println("Skipping it...");
-								//System.out.println();
-							}
-						} else {
-							//System.out.println(g + " Denied");
-						}
-					}
-				}
-			}
-			
-			//Old printwriter rip
-			//Now prints out all correct decrypted strings
-			try {
-				//PrintWriter pw = new PrintWriter(new FileWriter("output.txt"));
-				for(String g : confirmed) {
-					g = g.replaceAll("\n", "");
-					System.out.println("String Decoded: " + g);
-					/*g = g.replaceAll("\n", "");
-					pw.write(g);
-					pw.write("\n"); */
-				}
-				//pw.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}	
-			
-			//Outputs all of the decrypted strings to doutput.txt
-			try {
-				PrintWriter pw = new PrintWriter(new FileWriter("doutput.txt"));
-				for(String g : arr) {
-					g = g.replaceAll("\n", "");
-					pw.write(g);
-					pw.write("\n");
-				}
-				pw.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}	
+			br.close();
 		} catch (Exception e) {
 			System.out.println("Something went wrong...");
+		}
+	}
+	
+	public void checkEncryptedStrings() {
+		for(int i = 0; i<arr.size(); i++) {
+			String g = arr.get(i);
+			for(String a : words) {
+				StringTokenizer st = new StringTokenizer(g);
+				while(st.hasMoreTokens()) {
+					if(st.nextToken().equals(a)) {
+						if(!(confirmed.contains(g))) {
+							String l = g;
+							l = l.replaceAll("\n", "");
+							confirmed.add(g);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public void outputConfirmedStrings() {
+		for(String g : confirmed) {
+			g = g.replaceAll("\n", "");
+			System.out.println("String Decoded: " + g);
+		}
+	}
+	
+	public void debugOutput() {
+		try {
+			PrintWriter pw = new PrintWriter(new FileWriter("doutput.txt"));
+			for(String g : arr) {
+				g = g.replaceAll("\n", "");
+				pw.write(g);
+				pw.write("\n");
+			}
+			pw.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String args[]) {
+		Decryption dec = new Decryption();
+		dec.readLinesFromEncyptedFile();
+		dec.pullFromDictionary();
+		dec.checkEncryptedStrings();
+		dec.outputConfirmedStrings();
+		dec.debugOutput();
 	}
 }
